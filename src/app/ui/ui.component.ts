09e2bfe54135c48c10d97cf366724ef4eac85037
GabrielHS19 <jesus.gh72@gmail.com>
2020-11-23 09:22:04 -0600

    add stuff

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {calcularIMC} from '../hombre/hombre';
-import {calcular} from '../hombre/hombre';
+import {diagnosticoHombre} from '../hombre/hombre';
+import {diagnosticoMujer} from '../mujer/mujer';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
@@ -10,23 +11,30 @@ export class UiComponent implements OnInit {
 
   constructor() { }
 
-  result = 0;
+  result = '';
   result2='';
   operator1 = 0;
   operator2 = 0;
   operator3 = 0;
+  operator4='';
   ngOnInit(): void {
   }
 
   calcularIMC(){
+    let Resultado = '';
     let myresult = 0;
+    let myresultTxt = '';
     myresult = calcularIMC(this.operator2, this.operator1);
-    this.result = myresult;
+    if(this.operator4=='hombre'){
+      myresultTxt = diagnosticoHombre(this.operator3, myresult);
+    }else if (this.operator4=='mujer'){
+      myresultTxt = diagnosticoMujer(this.operator3, myresult);
+    }else{
+      myresultTxt='INVALIDO';
+    }
+    Resultado = `IMC: ${myresult.toFixed(2)} 
+     Diagnostico: ${myresultTxt}`
+    this.result = Resultado;
   }
 
-  calcular(){
-    let myresult = '';
-    myresult = calcular(this.operator3, calcularIMC(this.operator2, this.operator1));
-    this.result2 = myresult;
-  }
 }
